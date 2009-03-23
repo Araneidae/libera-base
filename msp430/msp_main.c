4f82cdab3e35c5e1fe12132c6a7b86a565036646
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2009-03-23 11:33:21 +0000

    Libera Base MSP driver.  Good working version.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/targetOS/libera-base@29780 e099a375-04f9-0310-9d5f-a741eaff62e1

--- msp430/msp_main.c
+++ msp430/msp_main.c
@@ -1,34 +1,38 @@
-/* $Id: msp_main.c 2237 2008-10-21 09:24:47Z hslmatejf $ */
-
-//! \file msp.c 
-//! Implements MSP GNU/Linux driver for Libera
-
-/*
-MSP - MSP GNU/Linux device driver for Libera
-Copyright (C) 2004-2006 Instrumentation Technologies
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
-or visit http://www.gnu.org
-*/
+/* This file is part of the MSP device driver for Libera
+ * Copyright (C) 2004-2006 Instrumentation Technologies
+ * Copyright (C) 2009  Michael Abbott, Diamond Light Source Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * The Libera EPICS Driver is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Contact:
+ *      Dr. Michael Abbott,
+ *      Diamond Light Source Ltd,
+ *      Diamond House,
+ *      Chilton,
+ *      Didcot,
+ *      Oxfordshire,
+ *      OX11 0DE
+ *      michael.abbott@diamond.ac.uk
+ */
 
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/cdev.h>
 
 #include <asm/uaccess.h>
-#include <asm/delay.h>
+#include <linux/delay.h>
 
 #include <linux/fs.h>
 #include <linux/ioport.h>
@@ -42,56 +46,10 @@ or visit http://www.gnu.org
 #include <asm/arch/ssp.h>
 
 
-
-#define SSP_WRITE(reg, value) __raw_writel(value, ssp.ssp->mmio_base + reg)
-#define SSP_READ(reg) __raw_readl(ssp.ssp->mmio_base + reg)
-
-
-/* Default module parameters */
-#define MSP_IOBASE          0x10000000
-#define MSP_IORANGE         0x00000010
-
-/* Port for MSP reset. */
-#define MSP_RESET           0xe
-/* MSP reset control bits */
-#define MRR_PGM      0x04
-#define MRR_TCK      0x02
-#define MRR_NMI      0x01
-
-
-/* MSP protocol defines */
-#define MSP_BAUD_DIVISOR            0x0f
-#define MSP_PROTO_END               0x10
-#define MSP_PROTO_CONV_START        0x10
-#define MSP_PROTO_CONV_IN_PROGRESS  0xef
-#define MSP_PROTO_CONV_TEST         0xa5
-
-
-/* MSP ADC properties */
-#define MSP_ADC_BIT    12
-#define MSP_ADC_RANGE  (1 << MSP_ADC_BIT)  
-#define MSP_ADC_VREF   2500  // mV
-
-
-
-/* Major and minor numbers for device files */
-/* NOTE: Major numbers in ranges 60-63, 120-127 and 240-254 
-   are reserved for experimental use.
-   For the inclusion in the official linux kernel one has to
-   apply for the assignment of a unique major number. 
-*/
-#define MSP_MAJOR         241
-
-
 /** MSP GNU/Linux driver version */
 #ifndef RELEASE_VERSION
 #define RELEASE_VERSION "unknown"
 #endif
-
-#define MSP_NAME "msp"
-
-
-
 
 
 /* Resistor scaling coefficients */
@@ -107,17 +65,6 @@ struct msp_atom {
 };
 
 
-
-/* I/O memory base address and range - default values */
-static unsigned long iobase  = MSP_IOBASE;
-static const  unsigned long iorange = MSP_IORANGE;
-
-/* SSP device. */
-static struct ssp_dev ssp;
-
-
-/* Global MSP device */
-static struct msp_atom atom;
 
 /* Resistor scaling coefficients */
 static const struct res_coeff res_coeff[9] = {
@@ -139,31 +86,54 @@ MODULE_DESCRIPTION("Instrumentation Technologies MSP driver for Libera");
 MODULE_LICENSE("GPL");
 MODULE_SUPPORTED_DEVICE("msp");
 
-module_param(iobase, long, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-MODULE_PARM_DESC (iobase,
-    "I/O memory region base address (default 0x10000000)");
 
 
 
 
 
+/*****************************************************************************/
+/*                                                                           */
+/*                          MSP 430 Hardware Interface                       */
+/*                                                                           */
+/*****************************************************************************/
+
+
+#define CPLD_IOBASE         0x10000000
+#define MSP_RESET_OFFSET    0xe
+#define MSP_RESET_BASE      (CPLD_IOBASE + MSP_RESET_OFFSET)
 
 
 
-DEFINE_MUTEX(msp_mutex);
+/* MSP reset control bits */
+#define MRR_PGM      0x04
+#define MRR_TCK      0x02
+#define MRR_NMI      0x01
 
 
-/* Class used link this device into /sys. */
-static struct class * msp_class;
-/* Major and minor numbers allocated to this device. */
-static dev_t msp_dev;
-/* Character device representation of this device. */
-static struct cdev msp_cdev;
+/* MSP protocol defines */
+#define MSP_BAUD_DIVISOR            0x0f
+#define MSP_PROTO_CONV_START        0x10
+#define MSP_PROTO_CONV_IN_PROGRESS  0xef
+#define MSP_PROTO_CONV_TEST         0xa5
+
+
+/* MSP ADC properties */
+#define MSP_ADC_BIT    12
+#define MSP_ADC_RANGE  (1 << MSP_ADC_BIT)  
+#define MSP_ADC_VREF   2500  // mV
 
 
 
 
-/** Efficient (non-busy) uninterruptible delay in jiffies */
+/* SSP device. */
+static struct ssp_dev ssp;
+/* MSP reset register (in CPLD address space). */
+static void * msp_reset_register;
+
+
+
+/* Efficient (non-busy) uninterruptible delay in jiffies.  This delays for
+ * about 5ms in general, but is a *lot* faster than calling mdelay(1)! */
 static void msp_delay_jiffies(int delay_jiff)
 {
     set_current_state(TASK_UNINTERRUPTIBLE);
@@ -172,27 +142,27 @@ static void msp_delay_jiffies(int delay_jiff)
 
 
 
-
-/** Reset MSP */
+/* Reset MSP */
 static void msp_reset(void)
 {
-    writel( ((MRR_PGM | MRR_TCK) & ~MRR_NMI), iobase + MSP_RESET);
+    iowrite32((MRR_PGM | MRR_TCK) & ~MRR_NMI, msp_reset_register);
     msp_delay_jiffies(1);
 
-    writel( (MRR_PGM | MRR_TCK |  MRR_NMI), iobase + MSP_RESET);
+    iowrite32(MRR_PGM | MRR_TCK | MRR_NMI, msp_reset_register);
     msp_delay_jiffies(1);
 
-    writel( ((MRR_NMI | MRR_TCK) & ~MRR_PGM), iobase + MSP_RESET);
+    iowrite32((MRR_NMI | MRR_TCK) & ~MRR_PGM, msp_reset_register);
     msp_delay_jiffies(1);
 }
 
 
-/** Transform ADC counts to power supply voltage */
+/* Transform ADC counts to power supply voltage */
 static void msp_transform(
     const unsigned long const *data, struct msp_atom *atom)
 {
     int i;
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++)
+    {
         atom->voltage[i] = (data[2*i+1] << 8) | data[2*i];
         // Two-step scaling to prevent overflow
         atom->voltage[i] =
@@ -205,68 +175,44 @@ static void msp_transform(
 }
 
 
-/** Wait for empty SSP TX fifo */
-static void ssp_sync_tx(void)
-{
-    while ( !(SSP_READ(SSSR) & SSSR_TFS) ) {
-        msp_delay_jiffies(1);
-    }
-    while (SSP_READ(SSSR) & SSSR_BSY) {
-        msp_delay_jiffies(1);
-    }
-}
-
-/** Wait for non-empty SSP RX fifo */
-static void ssp_sync_rx(void)
-{
-    while ( !(SSP_READ(SSSR) & SSSR_RFS) ) {
-        msp_delay_jiffies(1);
-    }
-    while (SSP_READ(SSSR) & SSSR_BSY) {
-        msp_delay_jiffies(1);
-    }
-}
+/* Our access to the SSP uses the IO configuration set up by ssp_init(), but
+ * we access the device directly as that driver's functionality is somewhat
+ * broken. */
+#define SSP_WRITE(reg, value) __raw_writel(value, ssp.ssp->mmio_base + reg)
+#define SSP_READ(reg) __raw_readl(ssp.ssp->mmio_base + reg)
 
 /* Exchange one word over SSP: send a word to the MSP, receive a word in
- * reply. */
+ * reply.  Because this is how we always work we know that the SSP TX FIFO
+ * will be empty, and we should get a reply pretty quickly. */
 static unsigned long ssp_exchange(unsigned long tx)
 {
-    unsigned long rx;
-
-    ssp_sync_tx();
-    SSP_WRITE(SSDR, tx & 0xffff);
-    ssp_sync_rx();
-    rx = SSP_READ(SSDR) & 0xffff;
-
-    msp_delay_jiffies(1);
-    
-    return rx;
+    SSP_WRITE(SSDR, tx);
+    while ((SSP_READ(SSSR) & SSSR_RNE) == 0)
+        msp_delay_jiffies(1);
+    return SSP_READ(SSDR) & 0xffff;
 }
 
 
-/** Read data from MSP */
+
+/* Read data from MSP */
 static ssize_t msp_transfer(char *buf)
 {
-    /* Initiate new transfer */
+    /* Initiate new transfer: send a conversion start command, and then wait
+     * for conversion to complete. */
     ssp_exchange(MSP_PROTO_CONV_START);
-    unsigned long rx_eoc;
-    while (rx_eoc = ssp_exchange(MSP_PROTO_CONV_TEST),
-           rx_eoc == MSP_PROTO_CONV_IN_PROGRESS ) {
+    while (ssp_exchange(MSP_PROTO_CONV_TEST) == MSP_PROTO_CONV_IN_PROGRESS)
         msp_delay_jiffies(1);
-    }
 
-    unsigned long rx = ssp_exchange(0);
-    if (rx != MSP_PROTO_CONV_TEST + 1) {
+    if (ssp_exchange(0) != MSP_PROTO_CONV_TEST + 1) 
         return -EBADE;
-    }
 
     unsigned long rx_buf[32];
     int j;
-    for (j = 0; j < MSP_PROTO_END; j++) {
+    for (j = 0; j < 16; j++) 
         rx_buf[j] = ssp_exchange(j + 1);
-    }
 
     /* Transform & deliver the received data */
+    struct msp_atom atom;
     msp_transform(rx_buf, &atom);
     if (copy_to_user(buf, &atom, sizeof(struct msp_atom)))
         return -EFAULT;
@@ -276,13 +222,100 @@ static ssize_t msp_transfer(char *buf)
 
 
 
-/** Called on open() system call.
- *
- * Takes care of proper opening of the MSP device and updates the information
- * for access control.
- *
- * For details see: Alessandro Rubini et al., Linux Device Drivers, pp. 63.
- */
+
+/* This initialises the MSP hardware resources.  These comprise:
+ *  1.  The SSP (Synchronous Serial Port) interface
+ *  2.  A register in the CPLD for resetting the MSP.
+ * We use as much of the kernel's own SSP interface as we can, but it's a bit
+ * broken. */
+static int initialise_msp(void)
+{
+    /* First try to capture the SSP resources. */
+    int ret = ssp_init(&ssp, 1, SSP_NO_IRQ);
+    if (ret < 0)
+    {
+        printk(KERN_ERR "Unable to initialise SSP resource: %d\n", -ret);
+        goto no_ssp;
+    }
+    /* Next, I/O memory resource allocation.  We only need the MSP reset
+     * register. */
+    if (request_mem_region(MSP_RESET_BASE, 4, "msp") == NULL)
+    {
+        printk(KERN_ERR "Unable to allocate MSP reset register\n");
+        ret = -ENODEV;
+        goto no_region;
+    }
+    /* Map the reset register. */
+    msp_reset_register = ioremap_nocache(MSP_RESET_BASE, 4);
+    if (msp_reset_register == NULL)
+    {
+        printk(KERN_ERR "Unable to remap MSP reset register\n");
+        ret = -ENODEV;
+        goto no_iobase;
+    }
+    
+
+    /* Enable the SSP clock. */
+    CKEN |= CKEN_SSP;
+    /* Configure all of the SSP GPIOs. */
+    pxa_gpio_mode(GPIO23_SCLK_MD);
+    pxa_gpio_mode(GPIO24_SFRM_MD);
+    pxa_gpio_mode(GPIO25_STXD_MD);
+    pxa_gpio_mode(GPIO26_SRXD_MD);
+    pxa_gpio_mode(GPIO27_SEXTCLK_MD);
+
+    /* Configure the SSP to talk to the MSP. */
+    ssp_disable(&ssp);
+    ssp_config(&ssp,
+        (SSCR0_DataSize(8) & SSCR0_DSS) | SSCR0_Motorola,
+        SSCR1_SPH, 0, SSCR0_SerClkDiv(32));
+    ssp_enable(&ssp);
+
+    /* Finally command the MSP to reset. */
+    msp_reset();
+    return 0;
+
+    
+no_iobase:
+    release_mem_region(MSP_RESET_BASE, 4);
+no_region:
+    ssp_exit(&ssp);
+no_ssp:
+    return ret;
+}
+
+
+static void uninitialise_msp(void)
+{
+    iounmap(msp_reset_register);
+    release_mem_region(MSP_RESET_BASE, 4);
+    ssp_exit(&ssp);
+}
+
+
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*                           Device Driver Interface                         */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/* Class used link this device into /sys. */
+static struct class * msp_class;
+/* Major and minor numbers allocated to this device. */
+static dev_t msp_dev;
+/* Character device representation of this device. */
+static struct cdev msp_cdev;
+
+
+DEFINE_MUTEX(msp_mutex);
+
+
+
+
+/* The only thing to do on open is to enforce read-only access. */
 static int msp_open(struct inode *inode, struct file *file)
 {
     if (file->f_mode & FMODE_WRITE)
@@ -292,10 +325,10 @@ static int msp_open(struct inode *inode, struct file *file)
 }
 
 
-/** Called on read() system call on a device.
- *
- * For details see: Alessandro Rubini et al., Linux Device Drivers, pp. 63.
- */
+/* We enforce reading a single block of readings from the MSP.  Also, because
+ * reading from the MSP takes such a long time, we make the lock
+ * interruptible (and it might be nice to allow the msp_transfer to be
+ * interruptible as well). */
 static ssize_t msp_read(
     struct file *file, char *buf, size_t count, loff_t *f_pos)
 {
@@ -312,36 +345,11 @@ static ssize_t msp_read(
 }
 
 
-static void initialise_msp(void)
-{
-    /* HW initialization */
-
-    /* Enable the SSP clock. */
-    CKEN |= CKEN_SSP;
-    pxa_gpio_mode(GPIO23_SCLK_MD);
-    pxa_gpio_mode(GPIO24_SFRM_MD);
-    pxa_gpio_mode(GPIO25_STXD_MD);
-    pxa_gpio_mode(GPIO26_SRXD_MD);
-    pxa_gpio_mode(GPIO27_SEXTCLK_MD);
-    
-    ssp_disable(&ssp);
-    ssp_config(&ssp,
-        (SSCR0_DataSize(8) & SSCR0_DSS) | SSCR0_Motorola,
-        SSCR1_SPH, 0, SSCR0_SerClkDiv(32));
-    ssp_enable(&ssp);
-
-    msp_reset();
-}
-
-
-/** MSP file operations 
- *
- * For details see: Alessandro Rubini et al., Linux Device Drivers, pp. 66.
- */
+/* MSP file operations. */
 static struct file_operations msp_fops = {
     owner:          THIS_MODULE,
-    read:           msp_read,
     open:           msp_open,
+    read:           msp_read,
 };
 
 
@@ -361,65 +369,58 @@ static int __init msp_init(void)
     printk(KERN_INFO "MSP version %s (%s %s)\n",
            RELEASE_VERSION, __DATE__, __TIME__);
 
-    int ret = ssp_init(&ssp, 1, SSP_NO_IRQ);
+    /* Initialise the device before doing anything else. */
+    int ret = initialise_msp();
     if (ret < 0)
-        goto no_ssp;
-
-    /* I/O memory resource allocation */
-    iobase = (unsigned) ioremap_nocache(iobase, iorange);
-    if ((void *)iobase == NULL)
-        goto no_iobase;
-    ret = check_mem_region(iobase, iorange);
-    if (ret < 0) 
-        goto no_class;
-    
-    ret = -ENODEV;  // in case of certain failures
-    if (request_mem_region(iobase, iorange, MSP_NAME) == NULL)
-        goto no_class;
-    
-    msp_class = class_create(THIS_MODULE, "msp");
-    if (msp_class == NULL)
-        goto no_class;
+        goto no_msp;
 
     /* Allocate ourself one device node. */
     ret = alloc_chrdev_region(&msp_dev, 0, 1, "msp");
     if (ret < 0)
+    {
+        printk(KERN_ERR "Unable to allocate dev for msp: %d\n", -ret);
         goto no_chrdev;
-    
+    }
+
+    /* Publish the newly created file operations.  At this point it is
+     * possible for the device to start being used. */
     cdev_init(&msp_cdev, &msp_fops);
     msp_cdev.owner = THIS_MODULE;
     ret = cdev_add(&msp_cdev, msp_dev, 1);
-    if (ret < 0) {
-        printk(KERN_ERR "Unable to register major %d, error %d\n",
-            MSP_MAJOR, -ret);
+    if (ret < 0)
+    {
+        printk(KERN_ERR "Unable to register msp device: %d\n", -ret);
         goto no_cdev;
     }
 
     /* Bind device to class: this places appropriate entries in /dev. */
-    ret = -ENODEV;  // in case of certain failures
+    msp_class = class_create(THIS_MODULE, "msp");
+    if (msp_class == NULL)
+    {
+        printk(KERN_ERR "Unable to create msp class\n");
+        ret = -ENODEV;
+        goto no_class;
+    }
     struct device * msp_device =
         device_create(msp_class, NULL, msp_dev, "msp0");
     if (msp_device == NULL)
+    {
+        printk(KERN_ERR "Unable to create msp device in class\n");
+        ret = -ENODEV;
         goto no_device;
-    
-
-    /* Module loaded OK */
-    initialise_msp();
+    }
     return 0;
     
 
 no_device:
+    class_destroy(msp_class);
+no_class:
     cdev_del(&msp_cdev);
 no_cdev:
     unregister_chrdev_region(msp_dev, 1);
 no_chrdev:
-    class_destroy(msp_class);
-no_class:
-    release_mem_region(iobase, iorange);
-    iounmap((void *)(iobase & PAGE_MASK));
-no_iobase:
-    ssp_exit(&ssp);
-no_ssp:
+    uninitialise_msp();
+no_msp:
 
     printk(KERN_ERR "Unable to initialise MSP module, error %d\n", -ret);
     return ret;
@@ -433,32 +434,13 @@ no_ssp:
  */
 static void __exit msp_exit(void) 
 {
-    /* Remove msp0. */
     device_destroy(msp_class, msp_dev);
-    /* Delete the underlying character device. */
-    cdev_del(&msp_cdev);
-    /* Finally we can let the device numbers go. */
-    unregister_chrdev_region(msp_dev, 1);
-    /* The device class is no longer useful. */
     class_destroy(msp_class);
-
-    /* Release I/O memory regions */
-    release_mem_region(iobase, iorange);
-    iounmap((void *)(iobase & PAGE_MASK));
-
-    ssp_exit(&ssp);
+    cdev_del(&msp_cdev);
+    unregister_chrdev_region(msp_dev, 1);
+    uninitialise_msp();
 }
 
 
-/** Register module init and cleanup functions.
- *
- * Registers module initialization and cleanup functions.
- * NOTE: This is the modern mechanism for registering a module's 
- *       initialization and cleanup functions. Doing it this way instead of 
- *       using init_module() and cleanup_module() is crucial for possible 
- *       future inclusion into a monolythic linux kernel (not a module). 
- *
- * For details see: Alessandro Rubini et al., Linux Device Drivers, pp. 35.
- */
 module_init(msp_init);
 module_exit(msp_exit);
